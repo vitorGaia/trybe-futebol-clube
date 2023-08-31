80ad41ecce1654b580e2a407e39e9d911427ff7f
vitorGaia <vitorgaia0027@gmail.com>
2023-08-31 17:25:02 -0300

    pre publisher.

--- app/backend/nyc.config.js
+++ app/backend/nyc.config.js
@@ -6,7 +6,11 @@ module.exports = {
     'src/database/config',
     'src/database/migrations',
     'src/database/seeders',
-    'src/database/models'
+    'src/database/models',
+    'src/app.ts',
+    'src/server.ts',
   ],
   include: ['src/**/*.ts']
 };
+
+// prototypes
